c9ae772fbd3250a5ea0233d7af35075e73bbd038
TinusGa <tinuskg@stud.ntnu.no>
2025-05-06 11:28:16 +0200

    bollefisse

--- another_asQ_test.py
+++ another_asQ_test.py
@@ -21,14 +21,13 @@ warnings.simplefilter("ignore", FutureWarning)
 from matplotlib.animation import FuncAnimation
 import matplotlib.pyplot as plt
 
-
 problem_parameters = {
     "Number of time windows": 1, # No functionality for this yet
-    "Number of temporal processors": 4,
+    "Number of temporal processors": 4, # Optimal choice is the root of the number of time steps
     "Number of time steps": 5, # Number of time steps must fit into a list following [2^k+1, 2^k, ..., 2^k] where k is an integer and the list length is equal to the number of temporal processors.
     "dt": 0.001,
-    "nx": 15,
-    "ny": 15,
+    "nx": 32,
+    "ny": 32,
     "degree_space": 1,
     "theta": 1,
 }
@@ -41,12 +40,9 @@ ny = problem_parameters['ny']
 dt = problem_parameters['dt']
 degree_space = problem_parameters['degree_space']
 theta = problem_parameters['theta']
-#check_setup(problem_parameters)
 
 # Create a time partition and an ensemble communicator
 time_partition = create_time_partition(n_timesteps-1, temporal_processors)
-
-
 ensemble = create_ensemble(time_partition, comm=COMM_WORLD)
 
 # Create a mesh with nx+1 and ny+1 vertices
@@ -60,8 +56,9 @@ u0 = Function(V)
 # u0.project(cos(pi*x)*cos(2*pi*y))
 # bcs = []
 
-u0.project(sin(0.25*pi*x)*cos(2*pi*y))
 bcs = [DirichletBC(V, 0, sub_domain=1)]
+
+u0.project(sin(0.25*pi*x)*cos(2*pi*y))
 
 
 def form_mass(u, v):
@@ -80,30 +77,30 @@ aaoform = AllAtOnceForm(aaofunc,
                         bcs=bcs)
 
 # asQ solver parameters
-solver_parameters = {
-    'snes_type': 'ksponly',
-    'mat_type': 'mpiaij',
-    'ksp_type': 'richardson',
-    'ksp_max_it': 0, # Since Cyclic Reduction is a direct solver/method
-    #'ksp_rtol': 1e-12,
-    'ksp_monitor': None,
-    'ksp_converged_rate': None,
-    'pc_type': 'python',
-    'pc_python_type': 'CyclicReduction.CyclicReductionPC',
-    'cyclic_reduction_pc_factor_mat_solver_type': 'mumps',
-}
-
 # solver_parameters = {
-# 'snes_type': 'ksponly',
-# 'mat_type': 'matfree',
-# 'ksp_type': 'richardson',
-# 'ksp_rtol': 1e-12,
-# 'ksp_monitor': None,
-# 'ksp_converged_rate': None,
-# 'pc_type': 'python',
-# 'pc_python_type': 'asQ.CirculantPC',
-# 'circulant_block': {'pc_type': 'lu'},
-# 'circulant_alpha': 1e-4}
+#     'snes_type': 'ksponly',
+#     'mat_type': 'mpiaij',
+#     'ksp_type': 'richardson',
+#     'ksp_max_it': 0, # Since Cyclic Reduction is a direct solver/method
+#     #'ksp_rtol': 1e-12,
+#     'ksp_monitor': None,
+#     'ksp_converged_rate': None,
+#     'pc_type': 'python',
+#     'pc_python_type': 'CyclicReduction.CyclicReductionPC',
+#     'cyclic_reduction_pc_factor_mat_solver_type': 'mumps',
+# }
+
+solver_parameters = {
+'snes_type': 'ksponly',
+'mat_type': 'matfree',
+'ksp_type': 'richardson',
+'ksp_rtol': 1e-12,
+'ksp_monitor': None,
+'ksp_converged_rate': None,
+'pc_type': 'python',
+'pc_python_type': 'asQ.CirculantPC',
+'circulant_block': {'pc_type': 'lu'},
+'circulant_alpha': 1e-4}
 
 # solver_parameters = {
 #     'ksp_monitor': None,
@@ -172,7 +169,7 @@ PETSc.Sys.Print(f"DOF's space: {space_dofs}, DOF's time: {time_dofs}, DOF's tota
 
 start = time.time()
 
-aaofunc.initial_condition.assign(u0)
+aaofunc.assign(u0)
 for i in range(1):
     aaosolver.solve()
     #aaofunc.bcast_field(-1, aaofunc.initial_condition)
